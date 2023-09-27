ffac5d2c56a9f7a75e0eb9d9f308d1d19e8adad5
bZverok <bohdan.zverok@blackbird.io>
2023-09-27 16:46:49 +0300

    added connection validation

---
+++ Apps.Crowdin/Connections/ConnectionValidator.cs
@@ -0,0 +1,32 @@
+﻿using Apps.Crowdin.Api;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.Crowdin.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        var client = new CrowdinClient(authProviders);
+
+        try
+        {
+            await client.Languages.ListSupportedLanguages();
+
+            return new()
+            {
+                IsValid = true
+            };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
+    }
+}
