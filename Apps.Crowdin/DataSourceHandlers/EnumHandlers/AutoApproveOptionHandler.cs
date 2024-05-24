ecf91b910b0caab9e394e0f313ed998f5397b6a7
Cecilia Nistal <cecilia.nistal@centific.com>
2024-05-24 14:52:08 -0300

    Added pretransaltion options

---
+++ Apps.Crowdin/DataSourceHandlers/EnumHandlers/AutoApproveOptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+
+namespace Apps.Crowdin.DataSourceHandlers.EnumHandlers
+{
+    public class AutoApproveOptionHandler : IStaticDataSourceHandler
+    {
+            public Dictionary<string, string> GetData() => new()
+        {
+            { "All", "All" },
+            { "None", "None" },
+            { "PerfectMatchOnly", "Perfect match only" },
+            { "ExceptAutoSubstituted", "Except auto substituted" }
+        };
+    }
+}
--- Apps.Crowdin/Models/Request/Translation/PreTranslateRequest.cs
+++ Apps.Crowdin/Models/Request/Translation/PreTranslateRequest.cs
@@ -1,13 +1,19 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Crowdin.DataSourceHandlers;
+using Apps.Crowdin.DataSourceHandlers.EnumHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Crowdin.Models.Request.Translation;
 
 public class PreTranslateRequest
 {
+    [DataSource(typeof(LanguagesDataHandler))]
     [Display("Language IDs")] public IEnumerable<string> LanguageIds { get; set; }
 
     [Display("File IDs")] public IEnumerable<string> FileIds { get; set; }
 
+    [DataSource(typeof(MtEnginesDataHandler))]
     [Display("Engine ID")] public string? EngineId { get; set; }
 
     [Display("Duplicate translations")] public bool? DuplicateTranslations { get; set; }
@@ -17,5 +23,13 @@ public class PreTranslateRequest
 
     [Display("Translate with perfect match only")]
     public bool? TranslateWithPerfectMatchOnly { get; set; }
+    
+    [StaticDataSource(typeof(AutoApproveOptionHandler))]    
+    [Display("Auto approve option")]
+    public string? AutoApproveOption { get; set; }
+
+    [StaticDataSource(typeof(PreTranslationMethodHandler))]
+    [Display("Pre translation method")]
+    public string? Method { get; set; }
 
 }
