f6c92e59b015c39fb4307724fbca427800c91540
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-10 12:30:07 +0300

    Preparation for checkpoints

---
+++ Apps.Crowdin/Models/Request/Task/GetTaskOptionalRequest.cs
@@ -0,0 +1,9 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Crowdin.Models.Request.Task;
+
+public class GetTaskOptionalRequest
+{
+    [Display("Task ID")]
+    public string? TaskId { get; set; }
+}
