840b40c31faef45dc6f7dadb0eab95c5f685953d
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-21 19:52:00 +0200

    EnterpriseClient improved error handling

--- Tests.Crowdin/ValidatorTests.cs
+++ Tests.Crowdin/ValidatorTests.cs
@@ -1,5 +1,6 @@
 using Apps.Crowdin.Api.RestSharp.Basic;
 using Apps.Crowdin.Connections;
+using Apps.Crowdin.Constants;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Exceptions;
 using FluentAssertions;
@@ -12,100 +13,6 @@ namespace Tests.Crowdin;
 [TestClass]
 public class ValidatorTests : TestBase
 {
-    private TestCrowdinRestClient _client;
-
-    [TestInitialize]
-    public void Setup()
-    {
-        _client = new TestCrowdinRestClient();
-    }
-
-
-    [TestMethod]
-    public void ConfigureErrorException_EmptyContentAndEmptyErrorMessage_ShouldReturnException()
-    {
-        var response = new RestResponse
-        {
-            Content = "",
-            ErrorMessage = ""
-        };
-        Action act = () => _client.PublicConfigureErrorException(response);
-
-        act.Should().ThrowExactly<Exception>();
-    }
-
-    [TestMethod]
-    public void ConfigureErrorException_EmptyContentWithErrorMessage_ShouldReturnPluginApplicationException()
-    {
-        var response = new RestResponse
-        {
-            Content = "",
-            ErrorMessage = "Some error occurred."
-        };
-
-        Action act = () => _client.PublicConfigureErrorException(response);
-
-        act.Should().ThrowExactly<PluginApplicationException>();
-    }
-
-    [TestMethod]
-    public void ConfigureErrorException_ValidHtmlErrorResponse_ShouldReturnPluginApplicationException()
-    {
-        var htmlContent = "<html><head><title>Error</title></head><body><p>Something went wrong.</p></body></html>";
-        var response = new RestResponse
-        {
-            Content = htmlContent,
-            ContentType = "text/html"
-        };
-        Action act = () => _client.PublicConfigureErrorException(response);
-        act.Should().ThrowExactly<PluginApplicationException>();
-    }
-
-    [TestMethod]
-    public void ConfigureErrorException_ValidJsonErrorResponse_ShouldThrowPluginApplicationException()
-    {
-        var jsonContent = "{\"error\": {\"code\": \"123\", \"message\": \"Invalid configuration.\"}}";
-        var response = new RestResponse
-        {
-            Content = jsonContent,
-            ContentType = "application/json"
-        };
-        Action act = () => _client.PublicConfigureErrorException(response);
-
-        act.Should().ThrowExactly<PluginApplicationException>();
-    }
-
-    [TestMethod]
-    public void ConfigureErrorException_JsonErrorResponse_ShouldThrowPluginApplicationException()
-    {
-        var jsonContent = "{\"error\": {\"code\": \"123\", \"message\": \"Invalid configuration.\"}}";
-        var response = new RestResponse
-        {
-            Content = jsonContent,
-            ContentType = ""
-        };
-        Action act = () => _client.PublicConfigureErrorException(response);
-
-        act.Should().ThrowExactly<PluginApplicationException>();
-    }
-
-    [TestMethod]
-    public void ConfigureErrorException_UnknownError_ShouldThrowPluginApplicationException()
-    {
-        var unknownContent = "This is an unknown response format.";
-        var response = new RestResponse
-        {
-            Content = unknownContent,
-            ContentType = "application/unknown"
-        };
-
-        Action act = () => _client.PublicConfigureErrorException(response);
-        act.Should().Throw<PluginApplicationException>();          
-    }
-
-
-
-
     [TestMethod]
     public async Task ValidateConnection_ValidEnterpriseConnection_ShouldBeSuccessful()
     {
