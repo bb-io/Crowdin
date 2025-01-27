75aac8662c01a59f8b8942a730c5fa779324c7ae
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-27 19:40:36 +0200

    Added type filter to on task added webhook

--- Apps.Crowdin/Models/Request/Task/TaskAddedOptionalRequest.cs
+++ Apps.Crowdin/Models/Request/Task/TaskAddedOptionalRequest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Apps.Crowdin.DataSourceHandlers.EnumHandlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Crowdin.Models.Request.Task
@@ -12,7 +13,7 @@ namespace Apps.Crowdin.Models.Request.Task
     public class TaskAddedOptionalRequest
     {
         [Display("Task type")]
-        [DataSource(typeof(TaskTypeHandler))]
+        [StaticDataSource(typeof(TaskTypeHandler))]
         public string? Type { get; set; }
     }
 }
