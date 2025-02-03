69074fe80c66c51c139d25ce12b88fb5f22da899
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-03 18:03:37 +0200

    Cheking for laguage

---
+++ Tests.Crowdin/TaskTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.Crowdin.Actions;
+using Apps.Crowdin.Models.Request.Task;
+using Apps.Crowdin.Models.Request.Users;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Tests.Crowdin.Base;
+
+namespace Tests.Crowdin
+{
+    [TestClass]
+    public class TaskTests :TestBase
+    {
+        [TestMethod]
+        public async Task CreateTask_ReturnSucces()
+        {
+            var action = new TaskActions(InvocationContext,FileManager);
+
+            var input1 = new AssigneesRequest { ProjectId = "750225" };
+            var input2 = new AddNewTaskRequest { Type = "Translate", Title="Hello", LanguageId = "en", FileIds = ["16"]};
+
+            var result = await action.AddTask(input1,input2);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
