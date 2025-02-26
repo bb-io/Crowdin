20a7a5edfd47bb7aa6fdf09b4e76ce4000d37aa4
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-26 11:29:39 +0200

    Added after event

--- Apps.Crowdin/Webhooks/Models/Payload/Task/Wrapper/TaskStatusChangedWrapper.cs
+++ Apps.Crowdin/Webhooks/Models/Payload/Task/Wrapper/TaskStatusChangedWrapper.cs
@@ -1,6 +1,9 @@
-﻿namespace Apps.Crowdin.Webhooks.Models.Payload.Task.Wrapper;
+﻿using Newtonsoft.Json;
+
+namespace Apps.Crowdin.Webhooks.Models.Payload.Task.Wrapper;
 
 public class TaskStatusChangedWrapper
 {
+    [JsonProperty("data")]
     public TaskStatusChangedPayload Task { get; set; }
 }
