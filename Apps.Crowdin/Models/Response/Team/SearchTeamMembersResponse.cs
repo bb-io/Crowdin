819dba4f208e90f8c2da91a42c28c5a1b2d1dca9
ce-nistal <cecilia.nistal@blackbird.io>
2025-03-18 21:22:32 -0300

    Added List of IDs to Search team members

---
+++ Apps.Crowdin/Models/Response/Team/SearchTeamMembersResponse.cs
@@ -0,0 +1,14 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Crowdin.Api.Teams;
+
+namespace Apps.Crowdin.Models.Response.Team
+{
+    public class SearchTeamMembersResponse
+    {
+        [Display("Team members")]
+        public IEnumerable<TeamMember> Members { get; set; }
+
+        [Display("Members IDs")]
+        public IEnumerable<string> Ids { get; set; }
+    }
+}
