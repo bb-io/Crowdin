2ec4c50a94282f4cb8b0d61cc5f369e6208782cf
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-03-25 16:25:34 +0100

    Add polling mechanism for translation memory export status.

---
+++ Apps.Crowdin/Polling/ExportPollingList.cs
@@ -0,0 +1,54 @@
+﻿using Apps.Crowdin.Invocables;
+using Apps.Crowdin.Polling.Models;
+using Apps.Crowdin.Polling.Models.Requests;
+using Apps.Crowdin.Polling.Models.Responses;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Polling;
+using Crowdin.Api;
+
+namespace Apps.Crowdin.Polling;
+
+[PollingEventList]
+public class ExportPollingList(InvocationContext invocationContext) : AppInvocable(invocationContext)
+{
+    [PollingEvent("On TM Export Status Changed", Description = 
+        "")] //TODO: fill out description
+    public async Task<PollingEventResponse<PollingMemory, TranslationMemoryStatusResponse>> OnPreTranslationStatusChanged(
+        PollingEventRequest<PollingMemory> request,
+        [PollingEventParameter] TranslationMemoryExportStatusChangedRequest tmExportStatusChangedRequest)
+    {
+        var tmExportStatusResponse = await SdkClient.TranslationMemory.CheckTmExportStatus(Convert.ToInt32(tmExportStatusChangedRequest.TranslationMemoryId), tmExportStatusChangedRequest.ExportId);
+        if (request.Memory is null)
+        {
+            return new()
+            {
+                FlyBird = false,
+                Memory = new()
+                {
+                    LastPollingTime = DateTime.UtcNow,
+                    Triggered = request.Memory?.Triggered ?? false
+                }
+            };
+        }
+
+        var TmExportHasRightStatus = false;
+
+        if (tmExportStatusResponse.Status == OperationStatus.Finished)
+        {
+            TmExportHasRightStatus = true;
+        }
+
+        var triggered = TmExportHasRightStatus && !request.Memory.Triggered;
+
+        return new()
+        {
+            FlyBird = triggered,
+            Result = new TranslationMemoryStatusResponse(tmExportStatusResponse),
+            Memory = new()
+            {
+                LastPollingTime = DateTime.UtcNow,
+                Triggered = triggered
+            }
+        };
+    }
+}
