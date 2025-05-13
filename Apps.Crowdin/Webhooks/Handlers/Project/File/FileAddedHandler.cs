d1e1f2ccbcdf319109776ff6cc5be9c717401311
RiabushenkoA <ariabushenko@blackbird.io>
2025-05-13 21:35:03 +0300

    Bridgify webhooks

--- Apps.Crowdin/Webhooks/Handlers/Project/File/FileAddedHandler.cs
+++ Apps.Crowdin/Webhooks/Handlers/Project/File/FileAddedHandler.cs
@@ -1,4 +1,5 @@
-﻿using Apps.Crowdin.Webhooks.Handlers.Base;
+﻿using Apps.Crowdin.Webhooks.Bridge;
+using Apps.Crowdin.Webhooks.Handlers.Base;
 using Apps.Crowdin.Webhooks.Models.Inputs;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Webhooks;
@@ -6,7 +7,7 @@ using EventType = Crowdin.Api.Webhooks.EventType;
 
 namespace Apps.Crowdin.Webhooks.Handlers.Project.File;
 
-public class FileAddedHandler(InvocationContext invocationContext, [WebhookParameter(true)] ProjectWebhookInput input) : ProjectWebhookHandler(invocationContext, input)
+public class FileAddedHandler(InvocationContext invocationContext, [WebhookParameter(true)] ProjectWebhookInput input) : ProjectBridgeWebhookHandler(invocationContext, input)
 {
     protected override List<EventType> SubscriptionEvents => new() { EventType.FileAdded };
 
