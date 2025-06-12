9578a4a9c979851b38d9450b3796606eae1d40e1
RiabushenkoA <ariabushenko@blackbird.io>
2025-06-12 17:55:47 +0300

    Added File data handler and Target language data handler

--- Apps.Crowdin/Models/Request/Translation/DownloadFileTranslationRequest.cs
+++ Apps.Crowdin/Models/Request/Translation/DownloadFileTranslationRequest.cs
@@ -1,13 +1,17 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Crowdin.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Crowdin.Models.Request.Translation;
 
 public class DownloadFileTranslationRequest
 {
     [Display("File ID")]
+    [DataSource(typeof(FileDataHandler))]
     public string FileId { get; set; }
 
     [Display("Target language")]
+    [DataSource(typeof(ProjectTargetLanguageDataHandler))]
     public string TargetLanguage { get; set; }
 
     [Display("Skip untranslated strings?")]
