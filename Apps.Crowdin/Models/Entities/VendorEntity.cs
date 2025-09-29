706e405d33f28c755517ff6c85889ffdad713072
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-29 10:27:01 +0300

    add VendorEntity.Description nullability

--- Apps.Crowdin/Models/Entities/VendorEntity.cs
+++ Apps.Crowdin/Models/Entities/VendorEntity.cs
@@ -4,14 +4,14 @@ public class VendorEntity
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public string Status { get; set; }
 
     public VendorEntity(Vendor vendor)
     {
         Id = vendor.Id;
         Name = vendor.Name;
-        Description = vendor.Description;
+        Description = string.IsNullOrWhiteSpace(vendor.Description) ? null : vendor.Description;
         Status = vendor.Status.ToString().ToLower();
     }
 }
