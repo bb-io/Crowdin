05b618280cdd339f9da7413c979d20605e042a8c
RiabushenkoA <ariabushenko@blackbird.io>
2025-10-20 21:40:19 +0300

    Added filters for action 'Search files'

---
+++ Apps.Crowdin/DataSourceHandlers/EnumHandlers/FilePriorityHandler.cs
@@ -0,0 +1,18 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.Crowdin.DataSourceHandlers.EnumHandlers
+{
+    public class FilePriorityHandler : IStaticDataSourceItemHandler
+    {
+        public IEnumerable<DataSourceItem> GetData()
+        {
+            return new List<DataSourceItem>
+            {
+                new DataSourceItem("low", "Low"),
+                new DataSourceItem("normal", "Normal"),
+                new DataSourceItem("high", "High")
+            };
+        }
+    }
+}
---
+++ Apps.Crowdin/DataSourceHandlers/EnumHandlers/FileStatusHandler.cs
@@ -0,0 +1,18 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.Crowdin.DataSourceHandlers.EnumHandlers
+{
+    public class FileStatusHandler : IStaticDataSourceItemHandler
+    {
+        public IEnumerable<DataSourceItem> GetData()
+        {
+            return new List<DataSourceItem>
+            {
+                new DataSourceItem("active", "Active"),
+                new DataSourceItem("not_imported", "Not imported"),
+                new DataSourceItem("not_configured", "Not configured")
+            };
+        }
+    }
+}
