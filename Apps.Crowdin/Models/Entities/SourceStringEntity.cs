ebadfee34bbcc1885e15f8ec1eb5f81b0f290e53
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-26 15:14:20 +0200

    migrate string get endpoints to REST, add fields outputs and filters

--- Apps.Crowdin/Models/Entities/SourceStringEntity.cs
+++ Apps.Crowdin/Models/Entities/SourceStringEntity.cs
@@ -1,21 +1,43 @@
 ﻿using Blackbird.Applications.Sdk.Common;
 using Crowdin.Api.SourceStrings;
+using Newtonsoft.Json;
 
 namespace Apps.Crowdin.Models.Entities;
 
 public class SourceStringEntity
 {
-    [Display("ID")] public string Id { get; set; }
-    [Display("Project ID")] public string ProjectId { get; set; }
-    [Display("File ID")] public string FileId { get; set; }
-    [Display("Branch ID")] public string? BranchId { get; set; }
-    [Display("Directory ID")] public string? DirectoryId { get; set; }
-    [Display("Web URL")] public string? WebUrl { get; set; }
+    [Display("ID")] 
+    public string Id { get; set; }
+
+    [Display("Project ID")] 
+    public string ProjectId { get; set; }
+
+    [Display("File ID")] 
+    public string FileId { get; set; }
+
+    [Display("Branch ID")] 
+    public string? BranchId { get; set; }
+
+    [Display("Directory ID")] 
+    public string? DirectoryId { get; set; }
+
+    [Display("Web URL")] 
+    public string? WebUrl { get; set; }
     public string Identifier { get; set; }
     public string Text { get; set; }
     public string Type { get; set; }
     public string Context { get; set; }
-    [Display("Created at")] public DateTime CreatedAt { get; set; }
+
+    [Display("Created at")] 
+    public DateTime CreatedAt { get; set; }
+
+    [Display("Fields")]
+    public IEnumerable<FieldEntity> Fields => FieldsDict.Select(x => new FieldEntity(x.Key, x.Value));
+
+    [DefinitionIgnore, JsonProperty("fields")]
+    public Dictionary<string, object> FieldsDict { get; set; } = [];
+
+    public SourceStringEntity() { }
 
     public SourceStringEntity(SourceString sourceString)
     {
--- Apps.Crowdin/Models/Request/SourceString/ListStringsRequest.cs
+++ Apps.Crowdin/Models/Request/SourceString/ListStringsRequest.cs
@@ -1,13 +1,12 @@
 ﻿using Apps.Crowdin.DataSourceHandlers;
 using Apps.Crowdin.DataSourceHandlers.EnumHandlers;
-using Apps.Crowdin.Models.Request.Project;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Crowdin.Models.Request.SourceString;
 
-public class ListStringsRequest : ProjectRequest
+public class ListStringsRequest
 {
     [Display("Denormalize placeholders")] public bool? DenormalizePlaceholders { get; set; }
 
